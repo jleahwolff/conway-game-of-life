5bdffabc0e2bd38116f38be3c6c0343f91da42f7
Jessica Wolff <jleahwolff@gmail.com>
2020-06-24 14:28:43 -0500

    Working on calling the API, and managing state of the nodes.

--- src/hooks/gridHook.js
+++ src/hooks/gridHook.js
@@ -4,17 +4,17 @@ import React, { useState } from 'react';
 const numRows = 25;
 const numCols = 25;
 
+// useState function, so only running once when state initialized
+// ```By default, we're going to set everything to 0's or "dead"
+//     so to initialize array with the "dead" zeros -> 
+//       * generate array, and grab the
+//       * grab len of array
+//   2nd param of Arr.from is mapping value & key and 
+//   returning what value is going to be```
 const GridHook = () => {
-  // useState function, so only running once when state initialized
   const [grid, setGrid] = useState(() => {
     const rows = [];
     for (let i = 0; i < numRows; i++){
-    // ```By default, we're going to set everything to 0's or "dead"
-    //     so to initialize array with the "dead" zeros -> 
-    //       * generate array, and grab the
-    //       * grab len of array
-    //   2nd param of Arr.from is mapping value & key and 
-    //   returning what value is going to be```
       rows.push(Array.from(Array(numCols), () => 0));
     }
     return rows;
@@ -22,7 +22,28 @@ const GridHook = () => {
     console.log("logging grid", grid);
     console.table(grid);
 
-    return <div>Grid function</div>
+    return (
+    <div style = {{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`
+    }}>
+      {/* returning the grid. mapping the row arr, and creating a col */}
+      {/* // Getting index so we can set bg color: i for row, k for col */}
+      {/* Normally, you dont want to use index as key, but since the divs are unshiftable, its fine  */}
+      {grid.map((rows, i) =>
+        rows.map((col, k) => 
+        <div 
+        onClick ={() => {
+          grid[i][k] = 
+        }}
+        key = {`${i} - ${k}`}
+        style={{width: '20px', 
+          height: '20px', backgroundColor: grid[i][k] ? 'pink' : 'blue',
+          border: 'solid 1px black'
+        }} /> )
+      )}
+        </div>
+    )
 }
 
 export default GridHook;
